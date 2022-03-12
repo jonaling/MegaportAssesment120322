18b402706892251e39a4471d618183cf9835ee21
jonaling <jonaling00@gmail.com>
2022-03-12 14:05:02 +1100

    Created a maven project from a template

---
+++ MegaportSort/src/main/java/org/jling/megaport/MegaportSort/App.java
@@ -0,0 +1,13 @@
+package org.jling.megaport.MegaportSort;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
