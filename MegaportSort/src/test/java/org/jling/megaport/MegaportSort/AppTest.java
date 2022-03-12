7ca209d3580b81e4f2689bbc95b577549c9c6ee7
jonaling <jonaling00@gmail.com>
2022-03-12 23:21:00 +1100

    updated file compare tests

--- MegaportSort/src/test/java/org/jling/megaport/MegaportSort/AppTest.java
+++ MegaportSort/src/test/java/org/jling/megaport/MegaportSort/AppTest.java
@@ -13,6 +13,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.apache.commons.io.FileUtils;
 
 
 public class AppTest
@@ -107,13 +108,39 @@ public class AppTest
     }
     
     //test response for actual test file using collection sort
+	@Test
     public void TestFileCollectionSort() {
-    	
+		String[] args = {"src/test/resources/test1.out"};
+		App.main(args);
+		File file1= new File("./output/test1-sorted.out");
+		File file2 = new File("src/test/resources/test1-sorted.out");
+		Boolean result;
+		try{
+			result = FileUtils.contentEquals(file1,file2);
+		}catch(Exception e){
+			e.printStackTrace();
+			result = false;
+		}
+		
+		Assertions.assertTrue(result);
     }
     
     //test response for actual test file using quick-sort
+	@Test
     public void TestFileQuickSort() {
-    	
+    	String[] args = {"src/test/resources/test2.in"};
+		App.main(args);
+		File file1= new File("./output/test2-sorted.in");
+		File file2 = new File("src/test/resources/test2-sorted.in");
+		Boolean result;
+		try{
+			result = FileUtils.contentEquals(file1,file2);
+		}catch(Exception e){
+			e.printStackTrace();
+			result = false;
+		}
+		
+		Assertions.assertTrue(result);
     }
     
     //test for file with no output
